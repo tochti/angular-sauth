64f449f1fbe749951888dcd187a374cd6e7cb24e
Tim Tochtermann <tim.tochtermann@gmail.com>
2015-10-07 22:55:59 +0200

    init

---
+++ test/karma.conf.js
@@ -0,0 +1,36 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'ext/jquery/dist/jquery.min.js',
+      'ext/angular/angular.js',
+      'ext/angular-mocks/angular-mocks.js',
+      'ext/angular-route/angular-route.js',
+      'ext/angular-resource/angular-resource.js',
+      'ext/angular-ui/ui-bootstrap-tpls-0.13.0.min.js',
+      'ext/angular-pdf-viewer/angular-pdf-viewer.min.js',
+      'js/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            //'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
